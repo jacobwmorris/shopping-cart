c61178e3b9b7e061a115b52f01222886eb9bb6c9
jacobwmorris <jacobwmorris0@gmail.com>
2023-05-19 15:14:24 -0400

    Add prices to products

--- src/components/helpers/products.js
+++ src/components/helpers/products.js
@@ -9,21 +9,21 @@ import waterPotion from "../../images/WaterPotion.png"
 import alcoholPotion from "../../images/AlcoholPotion.png"
 import sharkPotion from "../../images/SharkPotion.png"
 
-function Product(id, name, desc, image) {
-    return {id, name, desc, image}
+function Product(id, name, desc, price, image) {
+    return {id, name, desc, price, image}
 }
 
 const products = [
-    Product(0, "Energy Potion", "The famous Ye Olde Ruddy Bull brew.", energyPotion),
-    Product(1, "Speed potion", "Running behind? Don't forget one of these!", speedPotion),
-    Product(2, "Strength potion", "Make your friends think you work out!", strengthPotion),
-    Product(3, "Invisibility potion", "Great for social anxiety.", invisPotion),
-    Product(4, "Health potion", "The cure for what ails ya.", healthPotion),
-    Product(5, "Love potion", "Become irresistable!", lovePotion),
-    Product(6, "Luck potion", "Don't let your poker buddies know you have this.", luckPotion),
-    Product(7, "Hydration potion", 'For it is written: "Hydrate lest thou die-drate" (Definitely not just water).', waterPotion),
-    Product(8, "Befuddlement potion", 'Often used "recreationally" (Definitely not just whiskey).', alcoholPotion),
-    Product(9, "Shark repellant potion", "Hey, better safe than sorry!", sharkPotion)
+    Product(0, "Energy Potion", "The famous Ye Olde Ruddy Bull brew.", 1.99, energyPotion),
+    Product(1, "Speed potion", "Running behind? Don't forget one of these!", 4.99, speedPotion),
+    Product(2, "Strength potion", "Make your friends think you work out!", 4.99, strengthPotion),
+    Product(3, "Invisibility potion", "Great for social anxiety.", 14.99, invisPotion),
+    Product(4, "Health potion", "The cure for what ails ya.", 9.99, healthPotion),
+    Product(5, "Love potion", "Become irresistable!", 14.99, lovePotion),
+    Product(6, "Luck potion", "Don't let your poker buddies know you have this.", 24.99, luckPotion),
+    Product(7, "Hydration potion", 'For it is written: "Hydrate lest thou die-drate" (Definitely not just water).', 1.99, waterPotion),
+    Product(8, "Befuddlement potion", 'Often used "recreationally" (Definitely not just whiskey).', 14.99, alcoholPotion),
+    Product(9, "Shark repellant potion", "Hey, better safe than sorry!", 4.99, sharkPotion)
 ]
 
 export default products
